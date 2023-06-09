fe3a0d3725930e7041b06485037947168bbedcb3
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-06-09 15:57:16 -0300

    GalToLit

---
+++ Capitulo 1/Primeiros Códigos/GalToLit.java
@@ -0,0 +1,11 @@
+public class GalToLit {
+    public static void main(String[] args) {
+        double gallons; // contém o númeor de galões
+        double liters; // contém a conversão para litros
+    
+        gallons = 10; // começa com 10 galões
+        liters = gallons * 3.7854; // converte para litros
+    
+        System.out.println(gallons + " gallons is " + liters + " liters.");
+    }
+}
