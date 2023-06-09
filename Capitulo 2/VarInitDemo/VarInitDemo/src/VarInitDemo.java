6a6b99f0b8a3f928b788c1c9e6700219d86c4911
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-06-09 18:31:35 -0300

    Varinit

---
+++ Capitulo 2/VarInitDemo/VarInitDemo/src/VarInitDemo.java
@@ -0,0 +1,11 @@
+public class VarInitDemo {
+    public static void main(String[] args){
+        int x;
+        x = 10;
+        System.out.println(x);
+        x = 100;
+        System.out.println(x);
+        int y = 1000;
+        System.out.println(y);
+    }
+}
