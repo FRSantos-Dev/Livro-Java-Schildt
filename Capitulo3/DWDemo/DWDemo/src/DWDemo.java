85a3207b5065afe326c7af4a8a7ffb97de3e364c
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-09-05 16:08:17 -0300

    DWDemo created

---
+++ Capitulo3/DWDemo/DWDemo/src/DWDemo.java
@@ -0,0 +1,14 @@
+
+class DWDEMO {
+
+    public static void main(String[] args) throws java.io.IOException {
+
+        char ch;
+
+        do {
+            System.out.println("Press a key followed by ENTER: ");
+            ch = (char) System.in.read(); // get a char
+        } while (ch != 'q');
+    }
+
+}
