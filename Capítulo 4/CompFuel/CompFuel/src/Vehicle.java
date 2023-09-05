80f389663c1ea4c6b285b46c8e4de66ec2733ac7
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-09-05 17:25:12 -0300

    Vehicle created

---
+++ Capítulo 4/CompFuel/CompFuel/src/Vehicle.java
@@ -0,0 +1,15 @@
+
+class Vehicle{
+
+    int passengers;
+    int fuelcap;
+    int mpg;
+
+    int range(){
+        return mpg * fuelcap;
+    }
+
+    double fuelneeded(int miles){
+        return (double) miles / mpg;
+    }
+}
